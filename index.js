323a19270d9fd3ddfcc96523450cd2dc05f55234
Notakachimoto <slavinskiy03@bk.ru>
2023-09-24 21:35:38 +0000

    first step done

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 export default function solution(content) {
   // BEGIN
-  console.log(content)
+  const splitim = content.split('\n');
+  const deleat = splitim.slice(2); 
+  console.log(deleat.length);
+
+  const arr = (content) => {
+    const elemen = { namee:content[0],place:content[1],type:content[2],time:content[3],danger:content[4]
+    } 
+    return elemen
+   }
+
+   const rows = deleat.map((row) => row 
+   .split('|') 
+    .filter((row) => row)) 
+   .map((array) => array  
+       .map((element) => element.trim()) 
+        ); 
+
+    const sortirovka = rows.map((row) => arr(row));
+    const spisok = sortirovka.map((row) => row.namee);
+    const upRegistor = spisok.map((row, index) => spisok.indexOf(row) === index)
+    console.log(result.sort());
   // END
 }
